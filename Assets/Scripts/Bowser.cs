bd0d75895fa7f423c629a846d940968a3ca80d97
Sergi Albalat <sergi.albalat.7e8@itb.cat>
2026-02-24 15:21:30 +0100

    BowserRotationFixedAndFireBallsAdded

--- Assets/Scripts/Bowser.cs
+++ Assets/Scripts/Bowser.cs
@@ -1,17 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(MoveBehaviour))]
 public class Bowser : MonoBehaviour
 {
+    public Stack<GameObject> bowserProjectiles = new Stack<GameObject>();
     private MoveBehaviour _mB;
     private bool _following = false;
     private bool _inRange = false;
     private Vector3 _playerPosition;
-    private Vector3 _containedPosition;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Transform shootPoint;
     private void Awake()
     {
         _mB = GetComponent<MoveBehaviour>();
         InvokeRepeating("Jump", 4, 4);
         InvokeRepeating("ChangeMovement", 3, 3);
+        InvokeRepeating("ShootFireball", 3.5f, 3.5f);
     }
     private void Jump()
     {
@@ -23,15 +28,16 @@ public class Bowser : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.gameObject.CompareTag("Player"))
-            return;
-        _inRange = true;
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _inRange = true;
+
+        }
     }
     private void OnTriggerExit(Collider other)
     {
-        if(!other.gameObject.CompareTag("Player"))
-            return;
-        _inRange = false;
+        if(other.gameObject.CompareTag("Player"))
+            _inRange = false;
     }
     private void OnTriggerStay(Collider other)
     {
@@ -41,13 +47,27 @@ public class Bowser : MonoBehaviour
     }
     private void Update()
     {
-        
         if (_following && _inRange)
         {
             _mB.MoveTo(_playerPosition);
         }else if(!_following && _inRange)
         {
             _mB.MoveAway(_playerPosition);
+        }
+    }
+    private void ShootFireball()
+    {
+        if (bowserProjectiles.Count == 0)
+        {
+            GameObject proj = Instantiate(projectile, shootPoint.position, transform.rotation);
+            proj.GetComponent<BowserProjectile>().bowser = this;
+        }
+        else
+        {
+            GameObject proj = bowserProjectiles.Pop();
+            proj.transform.position = shootPoint.position;
+            proj.transform.rotation = transform.rotation;
+            proj.SetActive(true);
         }
     }
 }
--- Assets/Scripts/FloatBehaviour.cs
+++ Assets/Scripts/FloatBehaviour.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 
@@ -11,6 +12,12 @@ public class FloatBehaviour : MonoBehaviour
     }
     public void FloatTo(Vector3 direction)
     {
+        _cC.Move(direction * floatSpeed * Time.deltaTime);
+    }
+    public void FloatForward()
+    {
+        Vector3 movement = new Vector3(0, 0, 1);
+        Vector3 direction = movement.x * transform.right + movement.z * transform.forward;
         _cC.Move(direction * floatSpeed * Time.deltaTime);
     }
 }

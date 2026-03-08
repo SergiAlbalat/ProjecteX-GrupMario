c83d9f74f9759e053f80a13d683a888dadea6ae7
Alex Laptop <alejandro.martin.7e8@itb.cat>
2026-03-08 21:01:45 +0100

    Baked lights and ambient occlusion

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,8 +24,6 @@ public class GameManager : MonoBehaviour
         gm = this;
         DontDestroyOnLoad(gameObject);
         SceneManager.sceneLoaded += OnSceneLoaded;
-        player.gameObject.SetActive(false);
-        player.gameObject.SetActive(true);
     }
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
